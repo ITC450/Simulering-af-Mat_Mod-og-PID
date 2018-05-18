63e088cdccc909dd85a3c24d016d944015f04f5d
Unknown <rkraye16@student.aau.dk>
2018-05-18 14:01:04 +0200

    two lines

--- driveplot.m
+++ driveplot.m
@@ -3,12 +3,18 @@ coor = load('drive.txt', '-ascii');
 
 x=coor(:,1);
 y=coor(:,2);
+x2=coor(:,3);
+y2=coor(:,4);
 
 figure
-plot(x,y,'Color',[1,0,0])
+plot(x,y,x2,y2,'r')
 axis equal
 
 grid on
 title('Drive')
 xlabel('x')
 ylabel('y')
+
+ylims = get(gca,'YLim');
+hold on
+plot([0 0], ylims, '-black')
